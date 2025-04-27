080e7ec61ba6debd4234cacde25f418b5d8636f2
nimaldon <nicolem_1020@icloud.com>
2025-04-27 16:03:33 -0700

    lab 6 changes before reflection

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5 js, nicoles transportation 
+// Author: Nicole Maldonado
+// Date: April 27, 2025
 
-// Constants
+// Define Variables
+myTransport = ["car", "bus"];
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+//create an object for my main ride
+var myMainRide = {
+  make: "Ford",
+  model: "Fiesta",
+  color: "White",
+  year: 2015,
+  age: function() {
+  return 2025 - this.year;
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//output
+document.writeln("My Transport:", myTransport, "</br>");
+//this trick allows us to write an object to the document
+document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide,null,'/t'),"</pre>");
